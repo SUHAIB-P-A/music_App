1cc7e4ae2b962fa27b26bb9799cdd44c6d01478c
thegreatcode1 <suhaibpa72@gmail.com>
2024-06-19 11:51:36 +0530

    add search function ,to perform searching operation in the list

--- backend_musicApp/linked_list_store_music.py
+++ backend_musicApp/linked_list_store_music.py
@@ -40,27 +40,45 @@ class LinkedList:
                 temp = temp.next
         if temp is None:
             return
-
+# joining list
         prev.next = temp.next
         temp = None
+
+    def searchlist(self,key):
+        temp = self.head
+        if temp.data == key:
+            print(temp.data)
+            return
+        else:
+            while temp.next is not None:
+                if temp.data == key:
+                    print(temp.data)
+                    break
+                prev = temp
+                temp = temp.next
+        if temp is None:
+            return
 
 
 if __name__ == "__main__":
     list = LinkedList()
     choice = 0
     while True:
-        print(" 1 insert list")
-        print(" 2 display list")
-        print(" 3 delete list")
-        print(" 4 exit")
-        choice = int(input("enter your choice"))
+        print(" 1 Insert list")
+        print(" 2 Display list")
+        print(" 3 Delete list")
+        print(" 4 search")
+        print(" 5 Exit")
+        choice = int(input("Enter your choice : "))
         if choice == 1:
-            list.insert_list(input("enter a value"))
+            list.insert_list(input("Enter a value : "))
         elif choice == 2:
             list.display_list()
         elif choice == 3:
-            list.delete_list(input("enter the delete value"))
+            list.delete_list(input("Enter the delete value : "))
         elif choice == 4:
+            list.searchlist(input("Enter the search element : "))
+        elif choice == 5:
             break
         else:
             print("invalid choice")
